09089c10ed0dd3cabf1a76db3919dac2694ad921
frogtube <u.pradere@gmail.com>
2017-10-27 10:26:52 +0200

    Recommiting previous modifications

--- Web/index.php
+++ Web/index.php
@@ -3,7 +3,15 @@ require '../vendor/autoload.php';
 
 use \MyFramework\Application;
 
+define('ROOT', dirname(__DIR__));
 
+// Starting application
 $app = new Application();
 $app->getController();
 $app->run();
+
+// PDOFactory initialization
+
+
+
+
